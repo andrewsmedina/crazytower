07a09d535217ba825fe5e0b765e463616f2ba50e
Andrews Medina <andrewsmedina@gmail.com>
2009-08-19 18:37:43 -0300

    validation for player name

--- crazytower/models.py
+++ crazytower/models.py
@@ -1,2 +1,11 @@
+class Player(object):
+    
+    def __init__(self, name):
+
+        if not name:
+            raise ValueError(u"Player name can not be null or empty")
+            
+        self.name = name
+
 class Game(object):
     pass
---
+++ tests/unit/test_player.py
@@ -0,0 +1,29 @@
+from crazytower.models import Player
+
+def test_can_create_player():
+    new_player = Player(name="andrews")
+    assert new_player
+
+def test_create_player_is_a_Player():
+    new_player = Player(name="andrews")
+    assert isinstance(new_player, Player)
+    
+def test_created_player_keeps_name():
+    new_player = Player(name="andrews")
+    assert new_player.name == "andrews"
+    
+def test_create_player_raises_on_null_name():
+    try:
+        new_player = Player(name=None)
+    except ValueError, error:
+        assert str(error) == u"Player name can not be null or empty"
+        return 
+    assert False
+    
+def test_create_player_raises_on_empty_name():
+    try:
+        new_player = Player(name="")
+    except ValueError, error:
+        assert str(error) == u"Player name can not be null or empty"
+        return 
+    assert False
