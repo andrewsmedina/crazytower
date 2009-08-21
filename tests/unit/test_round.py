fdad8ae2fa9fe6d8bf2eabf1d99353379e004da9
andrews <andrews@mulgore.(none)>
2009-08-21 17:02:07 -0300

    add tests for round

--- tests/unit/test_round.py
+++ tests/unit/test_round.py
@@ -3,4 +3,12 @@ from crazytower.models import Round
 def test_can_create_round():
     new_round = Round(number=1)
     assert new_round
+    
+def test_created_round_is_a_Round():
+    new_round = Round(number=1)
+    assert isinstance(new_round, Round)
+
+def test_created_round_keeps_number():
+    new_round = Round(number=1)
+    assert new_round.number == 1
 
